23453f79810743a19f01976d761d730c7d01a8ca
Fanis <iskhak.fanis@yandex.ru>
2026-03-24 12:19:45 +0300

    Fix Module_1 Homework_2

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,10 +16,15 @@ class MainActivity : AppCompatActivity() {
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+//  Using anonymus class as click handler
         val search_btn = findViewById<MaterialButton>(R.id.search_btn)
-        search_btn.setOnClickListener   {
-            Toast.makeText(this@MainActivity, "Нажали на Поиск!", Toast.LENGTH_SHORT).show()
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) { Toast.makeText(this@MainActivity, "Нажали на Поиск!", Toast.LENGTH_SHORT).show()
+            }
         }
+        search_btn.setOnClickListener(imageClickListener)
+
+//  Using lambda as click handler
         val library_btn = findViewById<MaterialButton>(R.id.library_btn)
         library_btn.setOnClickListener   {
             Toast.makeText(this@MainActivity, "Нажали на Медиатека!", Toast.LENGTH_SHORT).show()
