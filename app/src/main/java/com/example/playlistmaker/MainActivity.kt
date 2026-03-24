d41cb834c97587f9dd08310ab2940d153eb1d80e
Fanis <iskhak.fanis@yandex.ru>
2026-03-24 12:16:09 +0300

    Add Module_1 Homework_2

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,20 @@ class MainActivity : AppCompatActivity() {
 // Disable edge to edge for study simplicity
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val search_btn = findViewById<MaterialButton>(R.id.search_btn)
+        search_btn.setOnClickListener   {
+            Toast.makeText(this@MainActivity, "Нажали на Поиск!", Toast.LENGTH_SHORT).show()
+        }
+        val library_btn = findViewById<MaterialButton>(R.id.library_btn)
+        library_btn.setOnClickListener   {
+            Toast.makeText(this@MainActivity, "Нажали на Медиатека!", Toast.LENGTH_SHORT).show()
+        }
+        val settings_btn = findViewById<MaterialButton>(R.id.settings_btn)
+        settings_btn.setOnClickListener   {
+            Toast.makeText(this@MainActivity, "Нажали на Настройки!", Toast.LENGTH_SHORT).show()
+        }
+
 // Disable ViewCompat for study simplicity
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
